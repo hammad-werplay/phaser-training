5a0852d665e829cf116753bb8fe388278f9c76d5
Muhammad Hammad Afzal <hammad.afzal@werplay.com>
2025-07-14 16:13:50 +0500

    Initialize Seat Jam project

---
+++ 02_seat-jam/src/scenes/Game.js
@@ -0,0 +1,32 @@
+import * as Phaser from "../phaser/phaser-3.87.0-core.js";
+
+import { adStart, onCtaPressed, onAudioVolumeChange } from "../networkPlugin";
+export class Game extends Phaser.Scene {
+	constructor() {
+		super("Game");
+	}
+
+	init() {
+		console.log("%cSCENE::Game", "color: #fff; background: #f0f;");
+	}
+
+	/**
+	 * This is required specially for Mintegral & MRAID networks.
+	 * Do not remove if you are using those networks.
+	 */
+	adNetworkSetup() {
+		adStart();
+
+		// This is required for MRAID networks, you can remove if you are not using MRAID
+		onAudioVolumeChange(this.scene);
+	}
+
+	create() {
+		this.adNetworkSetup();
+
+		this.input.on("pointerdown", () => {
+			this.sound.play("sound_fx");
+			onCtaPressed();
+		});
+	}
+}
---
+++ 02_seat-jam/src/scenes/Preloader.js
@@ -0,0 +1,34 @@
+import * as Phaser from "../phaser/phaser-3.87.0-core.js";
+
+import { Base64Manager } from "../utils/Base64Manager.js";
+import { LoadBase64Audio } from "../utils/LoadBase64Audio.js";
+import { LoadBase64BitmapFont } from "../utils/LoadBase64BitmapFont.js";
+import { adReady } from "../networkPlugin";
+import { soundFxMP3 } from "../../media/audio_sound_fx.mp3.js";
+
+export class Preloader extends Phaser.Scene {
+	constructor() {
+		super("Preload");
+	}
+
+	init() {
+		console.log("%cSCENE::Preloader", "color: #fff; background: #f00;");
+	}
+
+	preload() {
+		//  Invoke the Base64Manager - pass in the current scene reference and a callback to invoke when it's done
+		Base64Manager(this, () => this.base64LoaderComplete());
+
+		LoadBase64Audio(this, [{ key: "sound_fx", data: soundFxMP3 }]);
+	}
+
+	create() {
+		//  This may run before the Loader has completed, so don't use in-flight assets here
+	}
+
+	base64LoaderComplete() {
+		adReady();
+
+		this.scene.start("Game");
+	}
+}
